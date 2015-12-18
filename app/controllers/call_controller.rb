151e740689f4ad904e3f4cb9c4aeabf857796bf9
Thomas Tran <ttran@pypestream.com>
2015-12-18 09:40:06 +0700

    support conference

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -9,16 +9,25 @@ class CallController < ApplicationController
 
   def twilio_reponse
     Twilio::TwiML::Response.new do |response|
+      response.Say "You have joined the conference."
+
       response.Dial callerId: params[:Caller] do |dial|
         if params.include?(:phoneNumber)
+          Rails.logger.info "Call phone number"
           dial.Number params[:phoneNumber]
-        else
+        elsif params.include?(:Conference)
+           Rails.logger.info "Call Conference"
+          conference = params[:Conference] || "Conference01"
+          dial.Conference conference,
+            waitUrl: "http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical",
+            muted:  "false",
+            startConferenceOnEnter: "true",
+            endConferenceOnEnter: "true"
+        else  #  conference
+          Rails.logger.info "Call Client"
           dial.Client(params[:To])
         end
       end
     end
   end
 end
-
-
-
