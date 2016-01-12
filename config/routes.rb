5d882dea026a4d430ace3edf3b8dedd8041fe3a8
SamNguyen <samk17cmutpm@gmail.com>
2016-01-12 16:35:18 +0700

    change get => post for conference room

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   post 'users/updatelocation' => 'users#update_location'
   post 'users/callphonenumbers' => 'users#callclient'
 
-  get 'users/callconference' => 'users#twilio'
+  post 'users/callconference' => 'users#twilio'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
