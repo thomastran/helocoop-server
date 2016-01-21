0e1ec81c83024da3d066c16c849a7f3e0e97b836
SamNguyen <samk17cmutpm@gmail.com>
2016-01-21 16:15:01 +0700

    test for random name on conference

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -197,9 +197,10 @@ class UsersController < ApplicationController
     users = User.where("available = ?", true)
     users.each do |user|
       distance = caculate_location(user_initial, user)
-      if distance < 10
-        distances.push(Distance.new(distance, user.phone_number, user.name, user.description, user.address))
-      end
+      distances.push(Distance.new(distance, user.phone_number, user.name, user.description, user.address))
+      # if distance < 10
+      #   distances.push(Distance.new(distance, user.phone_number, user.name, user.description, user.address))
+      # end
     end
     distances.sort! { |a,b| a.getMile <=> b.getMile }
     return distances.take(3)
