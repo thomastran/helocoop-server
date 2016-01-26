9abb8679b18f9224a289610b66f2b6701c88195b
SamNguyen <samk17cmutpm@gmail.com>
2016-01-26 09:03:26 +0700

    fix bug cannot edit data admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :phone_number, :email, :code, :address, :name, :created_at, :updated_at, :token, :latitude, :longitude, :available, :instance_id, :description
 #
 # or
 #
