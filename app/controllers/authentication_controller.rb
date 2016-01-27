b576fc0e718200c4d745e154e8b84237b4aaaf87
SamNguyen <samk17cmutpm@gmail.com>
2016-01-27 11:13:34 +0700

    fix bug cannot create new row in table authencation

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-  before_action :authenticate_authentication!
+  # before_action :authenticate_authentication!
   skip_before_filter :verify_authenticity_token
 
   def create
