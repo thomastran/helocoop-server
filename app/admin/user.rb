5ff8a63f43b001f4bde9d3452680d1cdeba01d00
SamNguyen <samk17cmutpm@gmail.com>
2016-01-27 17:31:35 +0700

    add log out link to sign out

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register User do
   before_action :authenticate_authentication!
-
-
+  actions :all, except: [:update, :destroy]
+  menu priority: 1, label: "Users Account" # so it's on the very left
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
   permit_params :phone_number, :email, :code, :address, :name, :created_at, :updated_at, :token, :latitude, :longitude, :available, :instance_id, :description
+
 #
 # or
 #
