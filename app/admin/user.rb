382456a5eca44de025c9c26b18bc3e3bc18238ed
SamNguyen <samk17cmutpm@gmail.com>
2016-01-28 09:47:20 +0700

    Fix bug cannot update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
   before_action :authenticate_authentication!
-  actions :all, except: [:update, :destroy]
+  actions :all, except: [:destroy]
   menu priority: 1, label: "Users Account" # so it's on the very left
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :authentications, controllers: {
         sessions: 'authentication/sessions'
   }
-
   # devise_for :authentications
-  ActiveAdmin.routes(self)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
