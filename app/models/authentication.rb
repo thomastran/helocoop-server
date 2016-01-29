1fff8fb5a58528d18c7a15fea477dcf33b2d79a7
SamNguyen <samk17cmutpm@gmail.com>
2016-01-29 22:42:19 +0700

    add longitude latitude to table show for users

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,6 +1,6 @@
 class Authentication < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :timeoutable, :registerable,
+  devise :database_authenticatable, :timeoutable,
          :rememberable, :trackable, :validatable
 end
