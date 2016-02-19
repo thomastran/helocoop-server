e35c990c5032254fa045531a30d5b37e9683b9fd
SamNguyen <samk17cmutpm@gmail.com>
2016-02-19 15:35:10 +0700

    update migration

--- db/migrate/20160111063638_change_data_type_for_available.rb
+++ db/migrate/20160111063638_change_data_type_for_available.rb
@@ -1,5 +1,6 @@
 class ChangeDataTypeForAvailable < ActiveRecord::Migration
   def change
-    change_column :users, :available,  'boolean USING CAST(available AS boolean)'
+    # change_column :users, :available,  'boolean USING CAST(available AS boolean)'
+    change_column :users, :available,  :boolean
   end
 end
