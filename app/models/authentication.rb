2cf304a77e5ba8c69de50e0a458a5e296832ad5f
SamNguyen <samk17cmutpm@gmail.com>
2016-02-20 20:17:36 +0700

    add register to test

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,6 +1,6 @@
 class Authentication < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :timeoutable,
+  devise :database_authenticatable, :registerable, :timeoutable,
          :rememberable, :trackable, :validatable
 end
