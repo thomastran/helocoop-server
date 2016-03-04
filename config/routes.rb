62f28b779eb4a3f91c76b15eb1ecf86cd2a193c9
SamNguyen <samk17cmutpm@gmail.com>
2016-03-04 14:00:16 +0700

    fix 404

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rails.application.routes.draw do
   post 'voip/generatetoken' => 'voip#generate_token'
 
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Example of named routfe that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
